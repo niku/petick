cb7a11f89a14dad7b3a0f8988274c814d4a5adbe
niku <niku@niku.name>
2016-02-04 23:47:58 +0900

    Use ex_doc

--- mix.exs
+++ mix.exs
@@ -28,6 +28,7 @@ defmodule Petick.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
   end
 end
