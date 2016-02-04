6647b377af3de60c883072eb4ac33163123ba9ee
niku <niku@niku.name>
2016-02-05 00:29:34 +0900

    Use dialyxir

--- mix.exs
+++ mix.exs
@@ -29,6 +29,7 @@ defmodule Petick.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.11", only: :dev}]
+     {:ex_doc, "~> 0.11", only: :dev},
+     {:dialyxir, "~> 0.3", only: [:dev]}]
   end
 end
