3a9780ce62c21c0ce220dbd9abccae265e51a1a0
niku <niku@niku.name>
2018-02-20 15:54:03 +0900

    Support Elixir1.6 or later

--- mix.exs
+++ mix.exs
@@ -1,34 +1,23 @@
-defmodule Petick.Mixfile do
+defmodule Petick.MixProject do
   use Mix.Project
 
   def project do
     [app: :petick,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     version: "0.1.0",
+     elixir: "~> 1.6",
+     start_permanent: Mix.env() == :prod,
      deps: deps,
      description: description,
      package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
+  # Run "mix help compile.app" to learn about applications.
   def application do
-    [applications: [:logger],
+    [extra_applications: [:logger],
      mod: {Petick, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [{:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11", only: :dev},
